e041c1864f42a3ce1e897baa4bf8544a71e42a99
GishenCBoraluwa <95617712+GishenCBoraluwa@users.noreply.github.com>
2022-02-02 08:44:55 +0530

    pop up code

--- page 01/app.js
+++ page 01/app.js
@@ -7,3 +7,59 @@ const mobileMenu = () => {
     menuLinks.classList.toggle('active');
 };
 menu.addEventListener('click', mobileMenu);
+
+//model pf popup js
+
+document.getElementById('log_in').addEventListener('click',
+function(){
+    document.querySelector('.bg_model').style.display ='flex';
+}
+);
+
+document.getElementById('close').addEventListener('click',
+function(){
+    document.querySelector('.bg_model').style.display ='none';
+}
+);
+
+document.getElementById('sign_In_model1').addEventListener('click',
+function(){
+    document.querySelector('.bg_model').style.display ='none';
+    document.querySelector('.bg_model2').style.display ='flex';
+}
+);
+
+document.getElementById('c_n_s_i').addEventListener('click',
+function(){
+    document.querySelector('.bg_model2').style.display ='none';
+    document.querySelector('.bg_model3').style.display ='flex';
+}
+);
+
+document.getElementById('close2').addEventListener('click',
+function(){
+    document.querySelector('.bg_model2').style.display ='none';
+}
+);
+
+document.getElementById('sign_up_model1').addEventListener('click',
+function(){
+    document.querySelector('.bg_model').style.display ='none';
+    document.querySelector('.bg_model3').style.display ='flex';
+}
+);
+
+document.getElementById('signin_mod3').addEventListener('click',
+function(){
+    document.querySelector('.bg_model3').style.display ='none';
+    document.querySelector('.bg_model2').style.display ='flex';
+}
+);
+
+document.getElementById('close3').addEventListener('click',
+function(){
+    document.querySelector('.bg_model3').style.display ='none';
+}
+);
+
+// end of model popup
