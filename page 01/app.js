e2b9882adff91f7db581145ba355b35bc530ae85
Rajith Lahiru <rajithlahiru7@gmail.com>
2022-02-15 12:34:24 +0530

    js added for close mobileMenu

--- page 01/app.js
+++ page 01/app.js
@@ -9,14 +9,61 @@ const mobileMenu = () => {
 };
 menu.addEventListener('click', mobileMenu);
 
+// close mobile menu
+
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active')
+    if(window.innerWidth <= 768 && menuBars){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.remove('active');
+    }
+};
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
+
+
 // show active menu when scrolling
 
 const highlightMenu = () => {
     const elem = document.querySelector('.highlight')
     const homeMenu = document.querySelector('#home-page')
-    const elem = document.querySelector('#about-page')
-    const elem = document.querySelector('#event-page')
+    const aboutMenu = document.querySelector('#about-page')
+    const eventMenu = document.querySelector('#event-page')
+    const targetMenu = document.querySelector('#SDG-page')
+    let scrollPos = window.scrollY
+
+
+// adds 'highlight' class to my menu items
+    if(window.innerWidth > 960 && scrollPos < 600){
+        homeMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        // eventMenu.classList.remove('highlight')
+        // target Menu.classList.remove('highlight')
+        return
+    }else if(window.innerWidth > 960 && scrollPos < 1400){
+        aboutMenu.classList.add('highlight')
+        homeMenu.classList.remove('highlight')
+        eventMenu.classList.remove('highlight')
+        return
+    }else if(window.innerWidth >960 && scrollPos < 2345){
+        eventMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        targetMenu.classList.remove('highlight')
+        return
+    }
+    else if(window.innerWidth > 2345 && scrollPos < 3000){
+        targetMenu.classList.add('highlight')
+        eventMenu.classList.remove('highlight')
+        return
+    }
+    if((elem && window.innerWidth < 960 && scrollPos < 600) || elem ){
+        elem.classList.remove('highlight')
+    }
 }
+
+window.addEventListener('scroll',highlightMenu)
+window.addEventListener('click', highlightMenu)
 
 
 
